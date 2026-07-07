9c18b3593c1e7b32249fc05f3b51cc7d599580fa
haseeburrehmanjs <kamranking786500@gmail.com>
2026-07-08 02:13:51 +0500

    feat: enhance student route with query parameters and improve comments

--- index.mjs
+++ index.mjs
@@ -1,20 +1,52 @@
 console.log("this is express server hello world");
 
+// what is rest api
+// rest api is input and output is json
+
 import express from "express";
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 app.get("/profile", (req, res) => {
   res.send("Hello Profile!" + " " + new Date());
 });
 
-// learn params in server api urk
+// learn params in server api url
 app.get("/student/:name/:age", (req, res) => {
   console.log("req.params.name", req.params.name);
   console.log("req.params.age", req.params.age);
+});
+
+// learn query parameter in server api url
+app.get("/student", (req, res) => {
+  console.log("req.query.name", req.query.name);
+  console.log("req.query.age", req.query.age);
+  console.log(req.body);
+
+  const students = [
+    {
+      name: "Haseeb Ur Rehman",
+      age: 19,
+      designation: "Frontend Developer",
+    },
+    {
+      name: "Muhammad Asharib",
+      age: 19,
+      designation: "Graphic Designer",
+    },
+  ];
+
+  // const filterStudent = students.filter((item) =>
+  //   item.name.toLowerCase().includes(req?.query.name.toLowerCase()),
+  // );
+
+  // res.send(filterStudent);
+  res.send(students);
 });
 
 app.listen(port, () => {
