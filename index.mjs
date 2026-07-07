100e6478872f988808f1137330236fa912b224ee
haseeburrehmanjs <kamranking786500@gmail.com>
2026-07-07 14:05:04 +0500

    chore: initialize project with package.json and express dependency

---
+++ index.mjs
@@ -0,0 +1,16 @@
+console.log("this is express server hello world");
+
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/profile", (req, res) => {
+  res.send("Hello Profile!" + " " + new Date());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
