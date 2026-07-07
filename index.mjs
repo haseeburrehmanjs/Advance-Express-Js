78bbe41a9f850741dec2b02844ae158f77043818
haseeburrehmanjs <kamranking786500@gmail.com>
2026-07-08 01:21:48 +0500

    feat: add student route with parameters and update dev script

--- index.mjs
+++ index.mjs
@@ -11,6 +11,12 @@ app.get("/profile", (req, res) => {
   res.send("Hello Profile!" + " " + new Date());
 });
 
+// learn params in server api urk
+app.get("/student/:name/:age", (req, res) => {
+  console.log("req.params.name", req.params.name);
+  console.log("req.params.age", req.params.age);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
