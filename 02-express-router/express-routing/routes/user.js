603e782ad3e4523d37c4964bb7428c5c672b79b2
haseeburrehmanjs <kamranking786500@gmail.com>
2026-07-12 13:31:26 +0500

    Remove user management component and related functionality from the frontend

--- 02-express-router/express-routing/routes/user.js
+++ 02-express-router/express-routing/routes/user.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { nanoid } from "nanoid";
+import { client } from "./mongodb.js";
 
 const userRouter = express.Router();
+const myDB = client.db("userDatabase");
+const myColl = myDB.collection("users");
 
 // create user
 const userList = [];
-userRouter.post("/", (req, res) => {
+userRouter.post("/", async (req, res) => {
   const { name, email, phone } = req.body;
   const NewUser = {
     id: nanoid(),
@@ -14,20 +17,47 @@ userRouter.post("/", (req, res) => {
     phone,
   };
 
-  userList.push(NewUser);
+  const result = await myColl.insertOne(NewUser);
+  console.log(result);
 
   res.send({
     message: "user created successfully",
-    data: NewUser,
+    data: result,
   });
 });
 
 // get all users
-userRouter.get("/", (req, res) => {
-  res.send({
-    message: "user list successfully fetch",
-    data: userList,
-  });
+userRouter.get("/", async (req, res) => {
+  try {
+    const result = myColl.find({});
+    const userList = await result.toArray();
+
+    res.status(200).send({
+      success: "user list successfully fetch",
+      data: userList,
+    });
+  } catch (error) {
+    console.log("server not reachable", error);
+    res.status(500).send({
+      error: "server not reachable",
+    });
+  }
+});
+
+// get single user
+userRouter.get("/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    console.log("Searching for user ID: ", userId);
+    const user = await myColl.find({ id: userId }).toArray();
+    res.status(200).send({
+      success: "user found successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.log("Error occurred:", error);
+    res.status(500).send({ success: false, message: "Server error" });
+  }
 });
 
 // update user details
